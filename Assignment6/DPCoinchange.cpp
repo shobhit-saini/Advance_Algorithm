19876aa8289bfa1048fa832c86c393493197d659
SHOBHIT <shobhitsaini95@gmail.com>
2019-09-22 23:13:39 +0530

    Update DPCoinchange.cpp

--- Assignment6/DPCoinchange.cpp
+++ Assignment6/DPCoinchange.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main()
 {
 	int i, j, k, N;
+	
 	cout << "Enter the amount:";
 	cin >> N;
+	
 	cout << "Enter the no. of denomination:";
 	cin >> k;
+	
 	cout << "Enter the denomination:";
 	int D[k];
 	int T[N+1];
@@ -15,7 +18,9 @@ int main()
 	{
 		cin >> D[i];
 	}
+	
 	T[0] = 0;
+	
 	for(i = 1; i <= N; i++)
 	{
 		T[i] = INT_MAX;
@@ -25,6 +30,7 @@ int main()
 	{
 		for(j = 0; j < k; j++)
 		{
+			//we will check which denomination will give minimum among all
 			if(((i - D[j]) >= 0) && T[i - D[j]] + 1 < T[i])
 			{
 				T[i] = T[i - D[j]] + 1;
