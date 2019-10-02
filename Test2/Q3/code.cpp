9cc7890434f7f4bbb7cb52f0af545e4c12e04bcf
SHOBHIT <shobhitsaini95@gmail.com>
2019-10-02 16:12:13 +0530

    Create code.cpp

---
+++ Test2/Q3/code.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	string s1, s2;
+	int i = 0, j 
+	= 0;
+	cout << "Enter the first string:";
+	cin >> s1;
+	cout << "Enter the second string:";
+	cin >> s2;
+	while(s1[i] != '\0' && s2[j] != '\0')
+	{
+		if(s1[i] == s2[j])
+		{
+			i++; 
+		}
+		j++;
+	}
+
+	if(i == strlen(s1.c_str()))
+	  cout << "TRUE";
+	 	else
+	  		cout << "FALSE";
+}
