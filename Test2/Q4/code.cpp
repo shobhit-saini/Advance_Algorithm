f318d993b801e8cfeeb17f9d4e875b98b900528d
SHOBHIT <shobhitsaini95@gmail.com>
2019-10-02 15:38:13 +0530

    Update code.cpp

--- Test2/Q4/code.cpp
+++ Test2/Q4/code.cpp
@@ -9,16 +9,24 @@ int main()
 	int start[m];
 	int end[m];
 	int result[m];
+	
+	//Enter the start time and end time
 	for(i = 0; i < m; i++)
 	{
 		cin >> start[i];
 		cin >> end[i];
 	}
+	
+	//Sort the start time and end time array
 	sort(start, start + m);
 	sort(end, end + m);
+	
 	i = 0; 
 	int j = 0; 
 	k = 0;
+	
+	/*Merge the two array into one array while merging remember one thing put one into new array
+	when you take element from start array otherwise 0*/
 	while(i < m && j < m)
 	{
 		if(start[i] <= end[j])
@@ -27,19 +35,22 @@ int main()
 			i++;
 			k++;
 		}
-		else
-		{
-			result[k] = 0;
-			j++;
-			k++;
-		}
+		
+    		else
+    		{
+    			result[k] = 0;
+    			j++;
+    			k++;
+    		}
 	}
+	
 	while(i < m)
 	{
 		result[k] = 1;
 		i++;
 		k++;
 	}
+	
 	while(j < m)
 	{
 		result[k] = 0;
@@ -48,6 +59,8 @@ int main()
 	}
 
 	int var = 0, max = 0;
+	
+	//if any new value of var is encounter then increment max value
 	for(i = 0; i < k; i++)
 	{
 	    if(result[i] == 1)
@@ -63,3 +76,4 @@ int main()
 	}
 	cout << max;
 }
+
