54797531bbde414027b4dc56e440c9f331830896
SHOBHIT <shobhitsaini95@gmail.com>
2019-10-02 15:16:55 +0530

    Update code.cpp

--- Test2/Q4/code.cpp
+++ Test2/Q4/code.cpp
@@ -1,43 +1,6 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int *Merge(int* start, int* end, int m)
-{
-	int result[m];
-	int i = 0, j = 0, k = 0;
-	while(i < m && j < m)
-	{
-		if(start[i] <= end[j])
-		{
-			result[k] = 1;
-			i++;
-			k++;
-		}
-		else
-		{
-			result[k] = 0;
-			j++;
-			k++;
-		}
-	}
-	while(i < m)
-	{
-		result[k] = 1;
-		i++;
-		k++;
-	}
-	while(j < m)
-	{
-		result[k] = 0;
-		j++;
-		k++;
-	}
-	for(int i = 0; i < m; i++)
-	{
-		cout << result[i];
-	}
-	return result;
-}
 int main()
 {
 	int m, i, k;
@@ -83,15 +46,20 @@ int main()
 		j++;
 		k++;
 	}
-	/*result = Merge(start, end, m);*/
-	int var = 0;
+
+	int var = 0, max = 0;
 	for(i = 0; i < k; i++)
 	{
-	   
-	    /*if(result[i] == 1)
+	    if(result[i] == 1)
+	    {
 	        var++;
-	            else if(var > 0)
-	                var--;*/
+	        if(var > max)
+	        {
+	            max++;
+	        }
+	     }
+	     else if(var > 0)
+	                var--;
 	}
-	cout << var;
+	cout << max;
 }
