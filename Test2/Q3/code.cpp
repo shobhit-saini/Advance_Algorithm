67ba16542d52cd6e17d51f13e6c1311627772902
SHOBHIT <shobhitsaini95@gmail.com>
2019-10-05 22:19:39 +0530

    Update code.cpp

--- Test2/Q3/code.cpp
+++ Test2/Q3/code.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main()
 {
 	string s1, s2;
-	int i = 0, j 
-	= 0;
+	int i = 0, j = 0;
+	
+	////////////////////////////INPUT/////////////////////////////////////////////////////////////
 	cout << "Enter the first string:";
 	cin >> s1;
 	cout << "Enter the second string:";
 	cin >> s2;
+	
 	while(s1[i] != '\0' && s2[j] != '\0')
 	{
 		if(s1[i] == s2[j])
